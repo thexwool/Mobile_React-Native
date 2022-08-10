83e2063db9ecd8015ffdbcd8babc3c0431548e46
thexwool <allan.dsm95@gmail.com>
2022-08-10 17:25:48 -0300

    commit

--- imhere/App.tsx
+++ imhere/App.tsx
@@ -1,9 +1,17 @@
 // import { Fragment } from "react"
 import Home from './src/screens/home'
+import { StatusBar } from 'react-native'
 
 export default function App(){
     return(
-        <Home />
+        <>
+            <StatusBar 
+                barStyle="light-content" 
+                backgroundColor="transparent"
+                translucent={true}
+            />
+            <Home />
+        </>
     )
 }
 
--- imhere/src/screens/home/index.tsx
+++ imhere/src/screens/home/index.tsx
@@ -1,16 +1,32 @@
-import { Text, View, TextInput, TouchableOpacity, ScrollView, FlatList } from "react-native"
+import { useState } from "react"
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from "react-native"
 import { styles } from './styles'
 import { Participant } from "../../components/Participant"
 
 export default function Home(){ 
-    const participants = ['Rodrigo', 'Vini', 'Diego', 'Allan', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João']
+    const [participants, setParticipants] = useState<string[]>([])
+    const [participantName, setParticipantName] = useState('')
 
     const handleParticipantAdd = () => {
-        console.log("Você clicou em adicionar")
+        if(participants.includes(participantName)){
+            return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome.')
+        }
+
+        setParticipants(prevState => [...prevState, participantName])
+        setParticipantName('')
     }
 
     const handleParticipantRemove = (name: string) => {
-        console.log(`Você clicou em remover ${name}`)
+        Alert.alert('Remover', `Deseja remover o participante ${name}?`, [
+            {
+                text: 'Sim',
+                onPress: () => Alert.alert("Participante deletado!")
+            },
+            {
+                text: 'Não',
+                style: 'cancel'
+            }
+        ])
     }
 
 
@@ -30,6 +46,8 @@ export default function Home(){
                     style={styles.input}
                     placeholder='Nome do participante'
                     placeholderTextColor='#6B6B6B'
+                    onChangeText={setParticipantName}
+                    value={participantName}
                 />
 
                 <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
